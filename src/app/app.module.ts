315c215e22728579cd35b971c16e9a85d4ff8727
cs.rajipo <cs.rajipo@gmail.com>
2018-10-10 08:14:56 +0530

    CSV file downloader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ExportDataProvider } from '../providers/export-data/export-data';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {File} from '@ionic-native/file';
+import {InAppBrowser} from '@ionic-native/in-app-browser';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {ExportDataProvider} from '../providers/export-data/export-data';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    ExportDataProvider
-  ]
+    declarations: [
+        MyApp,
+        HomePage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler},
+        ExportDataProvider,
+        File,
+        InAppBrowser
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/providers/export-data/export-data.ts
+++ src/providers/export-data/export-data.ts
@@ -1,69 +1,91 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {File} from '@ionic-native/file';
+import {InAppBrowser} from '@ionic-native/in-app-browser';
+import {normalizeURL} from "ionic-angular";
 
-/*
-  Generated class for the ExportDataProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ExportDataProvider {
-
-  constructor() {
-    console.log('Hello ExportDataProvider Provider');
-  }
-
-  public export2CSV(data: any[], columnDelimiter: string, filename: string): void {
-    const result = this.convertDataToCSV(data, columnDelimiter);
-    this.downloadCSV(result, filename);
-  }
-
-  private convertDataToCSV(data: any[], columnDelimiter: string = ','): string {
-
-    const lineDelimiter = '\n';
-    let fields: string[] = [];
-    for (const key in data[0]) {
-      if ('object' === typeof data[0][key]) {
-        const subFields: string[] = Object.keys(data[0][key]).map((k) => key + '.' + k);
-        fields = fields.concat(subFields);
-      } else {
-        fields.push(key);
-      }
+    
+    public export2CSV(data: any[], columnDelimiter: string, filename: string): void {
+        const result = this.convertDataToCSV(data, columnDelimiter);
+        this.downloadCSV(result, filename);
     }
-    let result = '';
-    result += fields.join(columnDelimiter);
-    result += lineDelimiter;
 
-    data.forEach(function (item) {
-      let ctr = 0;
-      fields.forEach(function (key) {
-        if (ctr > 0) {
-          result += columnDelimiter;
+    private convertDataToCSV(data: any[], columnDelimiter: string = ','): string {
+
+        const lineDelimiter = '\n';
+        let fields: string[] = [];
+        for (const key in data[0]) {
+            if ('object' === typeof data[0][key]) {
+                const subFields: string[] = Object.keys(data[0][key]).map((k) => key + '.' + k);
+                fields = fields.concat(subFields);
+            } else {
+                fields.push(key);
+            }
         }
-        if (item[key]) {
-          result += item[key];
-        } else {
-          const subKeys: string[] = key.split('.');
-          result += item[subKeys[0]][subKeys[1]];
-        }
-        ctr++;
-      });
-      result += lineDelimiter;
-    });
-    return result;
-  }
+        let result = '';
+        result += fields.join(columnDelimiter);
+        result += lineDelimiter;
 
-  private downloadCSV(result: string, filename: string = 'export.csv'): void {
-
-    if (!result.match(/^data:text\/csv/i)) {
-      result = 'data:text/csv;charset=utf-8,' + result;
+        data.forEach(function (item) {
+            let ctr = 0;
+            fields.forEach(function (key) {
+                if (ctr > 0) {
+                    result += columnDelimiter;
+                }
+                if (item[key]) {
+                    result += item[key];
+                } else {
+                    const subKeys: string[] = key.split('.');
+                    result += item[subKeys[0]][subKeys[1]];
+                }
+                ctr++;
+            });
+            result += lineDelimiter;
+        });
+        return result;
     }
-    const data: string = encodeURI(result);
 
-    const link: HTMLElement = document.createElement('a');
-    link.setAttribute('href', data);
-    link.setAttribute('download', filename);
-    link.click();
-  }
+    private downloadCSV(result: string, filename: string = 'export.csv'): void {
+
+        if (!result.match(/^data:text\/csv/i)) {
+            result = 'data:text/csv;charset=utf-8,' + result;
+        }
+        let blob = new Blob([result], {type: 'application/pdf'});
+
+        let options: any = {
+            location: 'yes',//Or 'no'
+            hidden: 'no', //Or  'yes'
+            clearcache: 'yes',
+            clearsessioncache: 'yes',
+            zoom: 'yes',//Android only ,shows browser zoom controls
+            hardwareback: 'yes',
+            mediaPlaybackRequiresUserAction: 'no',
+            shouldPauseOnSuspend: 'no', //Android only
+            closebuttoncaption: 'Close', //iOS only
+            disallowoverscroll: 'no', //iOS only
+            toolbar: 'yes', //iOS only
+            enableViewportScale: 'no', //iOS only
+            allowInlineMediaPlayback: 'no',//iOS only
+            presentationstyle: 'pagesheet',//iOS only
+            fullscreen: 'yes',//Windows only
+        };
+        this.file.writeFile(this.file.externalRootDirectory + "Download", filename, blob, {
+            replace: true
+        }).then((entity) => {
+            alert("file created at: " + JSON.stringify(entity));
+            alert(entity.nativeURL);
+           // window.open(encodeURI(entity.nativeURL), '_blank', 'location=yes');
+            const browser = this.iab.create(encodeURI(entity.nativeURL), '_blank', options);
+             browser.show();
+        }).catch((error) => {
+            alert("error creating file at :" + JSON.stringify(error));
+        });
+        /*const data: string = encodeURI(result);
+        const link: HTMLElement = document.createElement('a');
+        link.setAttribute('href', data);
+        link.setAttribute('download', filename);
+        link.click();*/
+    }
 
 }
